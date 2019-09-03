4992f3d6a73218452a26e98e5cb31baa4076908c
James Dorr <jamesdorr3@gmail.com>
2019-09-02 23:49:05 -0700

    Updates

---
+++ precache-manifest.91eec40678c20bbd185a5cc4628e8843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5432d34e55eb827a938e70ab6074de",
+    "url": "/bowling/index.html"
+  },
+  {
+    "revision": "451e576ca8c7ef712934",
+    "url": "/bowling/static/css/main.da3199f0.chunk.css"
+  },
+  {
+    "revision": "5efdf2946dba0ba3b6ea",
+    "url": "/bowling/static/js/2.07e697a2.chunk.js"
+  },
+  {
+    "revision": "451e576ca8c7ef712934",
+    "url": "/bowling/static/js/main.2d23ac98.chunk.js"
+  },
+  {
+    "revision": "89c7115e6fc6b6d97c2a",
+    "url": "/bowling/static/js/runtime~main.d9788219.js"
+  }
+]);
